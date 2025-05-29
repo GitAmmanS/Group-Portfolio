52db7696ab0eb8e9e0e8f611ff14c7475f636ed8
Amman Sajjad <ammansajjad42@gmail.com>
2025-05-29 10:14:50 +0500

    v2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,10 +9,10 @@ const Contact = () => {
       </div>
 
       <div className="flex flex-wrap gap-8 justify-center items-center">
-        <a href="mailto:amman@labxperts.com" className="flex items-center gap-3 text-lg hover:text-indigo-600 transition">
+        <a href="mailto:ammansajjad42@gmail.com" className="flex items-center gap-3 text-lg hover:text-indigo-600 transition">
           <Mail /> techa6026@gmail.com
         </a>
-        <a href="tel:+923001234567" className="flex items-center gap-3 text-lg hover:text-green-600 transition">
+        <a href="tel:+923259055292" className="flex items-center gap-3 text-lg hover:text-green-600 transition">
           <Phone /> +92 325 9055292
         </a>
       </div>
